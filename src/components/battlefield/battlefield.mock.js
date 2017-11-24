4d6fa05e57c25c9374afc8a3a4a46e038def5ae3
Nemeth Norbert <nnorb93@gmail.com>
2017-11-24 13:51:06 +0100

    Update mock datas

--- src/components/battlefield/battlefield.mock.js
+++ src/components/battlefield/battlefield.mock.js
@@ -1,181 +1,126 @@
-export const table = [
-  {
-    characters: [
-      'player2'
-    ],
-    index: 0
-  },
-  {
-    characters: [
-      'player1'
-    ],
-    index: 1
-  },
-  {
-    type: 'TELEPORT',
-    twinsPosition: 36,
-    id: '36_2_TELEPORT',
-    index: 2
-  },
-  {
-    characters: [
-      'player1',
-      'player2'
-    ],
-    index: 3
-  },
-  {
-    index: 4
-  },
-  {
-    index: 5
-  },
-  {
-    index: 6
-  },
-  {
-    index: 7
-  },
-  {
-    index: 8
-  },
-  {
-    type: 'TELEPORT',
-    twinsPosition: 40,
-    id: '9_40_TELEPORT',
-    index: 9
-  },
-  {
-    type: 'TELEPORT',
-    twinsPosition: 34,
-    id: '34_10_TELEPORT',
-    index: 10
-  },
-  {
-    index: 11
-  },
-  {
-    index: 12
-  },
-  {
-    index: 13
-  },
-  {
-    index: 14
-  },
-  {
-    index: 15
-  },
-  {
-    index: 16
-  },
-  {
-    index: 17
-  },
-  {
-    index: 18
-  },
-  {
-    index: 19
-  },
-  {
-    index: 20
-  },
-  {
-    index: 21
-  },
-  {
-    index: 22
-  },
-  {
-    index: 23
-  },
-  {
-    index: 24
-  },
-  {
-    index: 25
-  },
-  {
-    type: 'ROCKET',
-    id: '26_ROCKET',
-    index: 26
-  },
-  {
-    index: 27
-  },
-  {
-    index: 28
-  },
-  {
-    index: 29
-  },
-  {
-    index: 30
-  },
-  {
-    index: 31
-  },
-  {
-    index: 32
-  },
-  {
-    index: 33
-  },
-  {
-    type: 'TELEPORT',
-    twinsPosition: 10,
-    id: '34_10_TELEPORT',
-    index: 34
-  },
-  {
-    index: 35
-  },
-  {
-    type: 'TELEPORT',
-    twinsPosition: 2,
-    id: '36_2_TELEPORT',
-    index: 36
-  },
-  {
-    index: 37
-  },
-  {
-    index: 38
-  },
-  {
-    index: 39
-  },
-  {
-    type: 'TELEPORT',
-    twinsPosition: 9,
-    id: '9_40_TELEPORT',
-    index: 40
-  },
-  {
-    index: 41
-  },
-  {
-    index: 42
-  },
-  {
-    index: 43
-  },
-  {
-    index: 44
-  },
-  {
-    index: 45
-  },
-  {
-    type: 'ROCKET',
-    id: '46_ROCKET',
-    index: 46
-  },
-  {
-    index: 47
-  }
-];
+export const table = [{ characters: ['player1', 'player2'], index: 0 },
+{
+  type: 'portal',
+  twinsPosition: 13,
+  id: '13_1_portal',
+  value: 'portal1',
+  index: 1
+},
+{ index: 2 },
+{ index: 3 },
+{ index: 4 },
+{ index: 5 },
+{
+  type: 'spaceship',
+  id: '6_spaceship',
+  value: 'spaceship3',
+  index: 6
+},
+{
+  type: 'spaceship',
+  id: '7_spaceship',
+  value: 'spaceship2',
+  index: 7
+},
+{ index: 8 },
+{ index: 9 },
+{ index: 10 },
+{
+  type: 'portal',
+  twinsPosition: 40,
+  id: '40_11_portal',
+  value: 'portal2',
+  index: 11
+},
+{
+  type: 'spaceship',
+  id: '12_spaceship',
+  value: 'spaceship1',
+  index: 12
+},
+{
+  type: 'portal',
+  twinsPosition: 1,
+  id: '13_1_portal',
+  value: 'portal1',
+  index: 13
+},
+{
+  type: 'portal',
+  twinsPosition: 30,
+  id: '14_30_portal',
+  value: 'portal5',
+  index: 14
+},
+{ index: 15 },
+{ index: 16 },
+{ index: 17 },
+{ index: 18 },
+{
+  type: 'portal',
+  twinsPosition: 39,
+  id: '19_39_portal',
+  value: 'portal4',
+  index: 19
+},
+{ index: 20 },
+{ index: 21 },
+{ index: 22 },
+{ index: 23 },
+{ index: 24 },
+{ index: 25 },
+{ index: 26 },
+{ index: 27 },
+{ index: 28 },
+{ index: 29 },
+{
+  type: 'portal',
+  twinsPosition: 14,
+  id: '14_30_portal',
+  value: 'portal5',
+  index: 30
+},
+{
+  type: 'portal',
+  twinsPosition: 35,
+  id: '35_31_portal',
+  value: 'portal3',
+  index: 31
+},
+{ index: 32 },
+{ index: 33 },
+{ index: 34 },
+{
+  type: 'portal',
+  twinsPosition: 31,
+  id: '35_31_portal',
+  value: 'portal3',
+  index: 35
+},
+{ index: 36 },
+{ index: 37 },
+{ index: 38 },
+{
+  type: 'portal',
+  twinsPosition: 19,
+  id: '19_39_portal',
+  value: 'portal4',
+  index: 39
+},
+{
+  type: 'portal',
+  twinsPosition: 11,
+  id: '40_11_portal',
+  value: 'portal2',
+  index: 40
+},
+{ index: 41 },
+{ index: 42 },
+{ index: 43 },
+{ index: 44 },
+{ index: 45 },
+{ index: 46 },
+{ index: 47 }];
 
 export const tableSize = {
   row: 6,
